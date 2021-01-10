8f34f533b9668d2fffa40756a20a389e1729f233
mori8 <kaithape@gmail.com>
2021-01-10 15:08:57 +0900

    제출한 날짜별로 구분되어 렌더링되도록 구현

---
+++ src/components/MainPage/FormWrapperByDate.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import FormListItem from './FormListItem';
+
+class FormWrapperByDate extends Component {
+    render() {
+        const dateStyle = {
+            margin: "3.2rem 1rem 0",
+            lineHeight: "1",
+            fontSize: "1.1em",
+            color: "#555"
+        }
+        return (
+            <div>
+                <h5 style={dateStyle}>{this.props.date}</h5>
+                {this.props.data.map((data) => {
+                    return <FormListItem info={data}/>
+                })}
+            </div>
+        );
+    }
+}
+
+export default FormWrapperByDate;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
+<React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
