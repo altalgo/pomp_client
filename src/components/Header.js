48aa7d141b9fc58dee0b971a1a292411808288d2
Junek3 <rlarudrms28@gmail.com>
2021-01-10 15:41:28 +0900

    Fix conflict

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,10 @@ import Logout from './Logout';
 import '../App.css';
 
 class Header extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div className='header'>
-        <Link to='/forms' className='logo'>
+        <Link to='/forms' className='header--logo'>
           <span style={{ color: '#333333' }}>Pom</span>
           <span style={{ color: '#92a8d1' }}>p</span>
         </Link>
@@ -22,24 +21,6 @@ class Header extends Component {
       </div>
     );
   }
-=======
-    render() {
-        return (
-            <div className="header">
-                <Link to="/forms" className="header--logo">
-                    <span style={{color: "#333333"}}>Pom</span>
-                    <span style={{color: "#92a8d1"}}>p</span>
-                </Link>
-                <div className="spacer"></div>
-                <div className="header--userinfo">
-                    <span className="header--username">{this.props.username}</span>
-                    <span className="header--logout">{this.props.logoutBtn === "true" ? <Logout/> : ''}</span>
-                </div>
-                
-            </div>
-        );
-    }
->>>>>>> 8f34f533b9668d2fffa40756a20a389e1729f233
 }
 
 export default Header;
