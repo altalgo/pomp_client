b4439243f87100994c707787667fee7217505160
dongzoolee <nant0313@gmail.com>
2021-01-14 00:11:02 +0900

    overflow scroll 제거

--- src/components/Form/Check.js
+++ src/components/Form/Check.js
@@ -9,20 +9,20 @@ export default class Check extends Component {
         <div className='img--wrapper'>
           <img src={this.props.imgUrl} alt='' />
         </div>
-        <hr className="component--hr"/>
+        <hr className="component--hr" />
         <div className='check--list'>
           {this.props.list.map((ele, idx) => {
             return this.props.ans.includes(ele) ? (
               <div className='checked' key={ele + idx.toString()}>
                 <input type='radio' checked disabled />
                 {ele}
-              </div>
-            ) : (
-              <div key={ele + idx.toString()}>
-                <input type='radio' disabled />
-                {ele}
-              </div>
-            );
+              </div>)
+              : (
+                <div key={ele + idx.toString()}>
+                  <input type='radio' disabled />
+                  {ele}
+                </div>
+              );
           })}
         </div>
       </div>
--- src/components/Form/Radio.js
+++ src/components/Form/Radio.js
@@ -3,30 +3,30 @@ import './Radio.css';
 import './Short.css';
 
 export default class Radio extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className='title'>{this.props.title}</div>
-        <div className='desc'>{this.props.desc}</div>
-        <div className='img--wrapper'>
-          <img src={this.props.imgUrl} alt='' />
-        </div>
-        <hr className="component--hr" />
-        <div className='radio--list'>
-          {this.props.list.map((ele, idx) => {
-            return ele === this.props.ans ? (
-              <div className='checked' key={idx.toString()}>
-                <input type='radio' checked disabled />
-                {ele}
-              </div>)
-              : (
-                <div key={idx.toString()}>
-                  <input type='radio' disabled />
-                  {ele}
-              </div>)
-          })}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='container'>
+                <div className='title'>{this.props.title}</div>
+                <div className='desc'>{this.props.desc}</div>
+                <div className='img--wrapper'>
+                    <img src={this.props.imgUrl} alt='' />
+                </div>
+                <hr className="component--hr" />
+                <div className='radio--list'>
+                    {this.props.list.map((ele, idx) => {
+                        return (ele === this.props.ans) ? (
+                            <div className='checked' key={'checked' + idx.toString()}>
+                                <input type='radio' checked disabled />
+                                {ele}
+                            </div>)
+                            : (
+                                <div key={'radio' + idx.toString()}>
+                                    <input type='radio' disabled />
+                                    {ele}
+                                </div>);
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
